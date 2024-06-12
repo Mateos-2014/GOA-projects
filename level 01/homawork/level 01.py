389f77b106aa39f93ddc789a688269ce97ad5a8d
mate-2014 <mate.tskhomelidze.mail@gmail.com>
2024-06-12 23:52:06 +0400

    second commit

---
+++ level 01/homawork/level 01.py
@@ -0,0 +1,11 @@
+name = "mate" 
+surname = "tskhomelidze"
+age = 10 
+height = 150  
+
+knows_programing = True   
+is_ugly = False 
+
+
+
+print(name + " " + surname + " " + str(age) + " " + str(height) + " " + str(knows_programing) + " " + str(is_ugly) )
