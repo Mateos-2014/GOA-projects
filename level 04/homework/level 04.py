2eaa56aad49632f258d6e85e781a86f98666d47c
mate-2014 <mate.tskhomelidze.mail@gmail.com>
2024-06-12 23:44:21 +0400

    second commit

---
+++ level 04/homework/level 04.py
@@ -0,0 +1,11 @@
+name = "mate"
+surename = "tskhomelidze"
+age = 10
+hobby = "play football"
+country = "Georgia"
+print (name + '' + surename + '' + age + ' ' + hobby + ' ' + country)
+print (name)
+print (surename)
+print (age)
+print (hobby)
+print (country)
