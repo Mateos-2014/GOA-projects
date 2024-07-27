4658c5a1343cbd13142440eda7735db24abf9ee4
mate-2014 <mate.tskhomelidze.mail@gmail.com>
2024-07-27 09:40:21 +0400

    second commit

---
+++ level 11/homework/level11..py
@@ -0,0 +1,6 @@
+age=int(input ("enter number:"))
+if age>=18:
+
+    print(True)
+else:     
+   print(False)
---
+++ level 11/homework/level11.py
@@ -0,0 +1,21 @@
+number1 = float (input("enter number 1:"))
+number2 = float (input("enter number 2:"))
+
+if number1==number2:
+     print(True)
+
+else:
+     print(False)
+
+
+
+
+
+
+
+
+
+
+
+
+
