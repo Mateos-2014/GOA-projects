5b32895755e5d47a792ba7a0599b4271e37b9696
mate-2014 <mate.tskhomelidze.mail@gmail.com>
2024-08-22 22:07:43 +0400

    commit

---
+++ level 14/classwork/level14 .py
@@ -0,0 +1,6 @@
+age = int(input("Enter Your age:"))
+age1 = 18
+age2 = 60
+type = 1
+work = int(input("Are You Working? if working type 1:"))
+print(age >= age1 , age <= age2 , work == type)
---
+++ level 14/classwork/level14.py
@@ -0,0 +1,4 @@
+number = int(input("Enter Your Number: "))
+my_age = 10
+
+print(number == my_age)
